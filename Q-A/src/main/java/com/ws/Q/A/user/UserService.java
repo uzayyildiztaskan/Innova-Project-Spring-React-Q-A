5aac6f00441a9708c5f4e6350439047bda95de58
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-10 14:03:07 +0300

    Password encoding

--- Q-A/src/main/java/com/ws/Q/A/user/UserService.java
+++ Q-A/src/main/java/com/ws/Q/A/user/UserService.java
@@ -1,5 +1,7 @@
 package com.ws.Q.A.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,12 +9,16 @@ public class UserService {
 	
 	UserRepository	userRepository;
 	
+	PasswordEncoder passwordEncoder;
+	
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
 	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);		
 	}
 	

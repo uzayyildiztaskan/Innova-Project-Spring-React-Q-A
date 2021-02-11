7faa78f9ee52a7e6a8b2cf6347303443d5812dd0
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-11 16:47:33 +0300

    Username confirmation

--- Q-A/src/main/java/com/ws/Q/A/user/User.java
+++ Q-A/src/main/java/com/ws/Q/A/user/User.java
@@ -1,5 +1,6 @@
 package com.ws.Q.A.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -19,6 +20,7 @@ public class User {
 	
 	@NotNull
 	@Size(min = 4, max = 255)
+	@UniqueUsername
 	private String username;
 	
 	@NotNull
--- Q-A/src/main/java/com/ws/Q/A/user/UserRepository.java
+++ Q-A/src/main/java/com/ws/Q/A/user/UserRepository.java
@@ -3,5 +3,7 @@ package com.ws.Q.A.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByUsername(String username);
 
 }

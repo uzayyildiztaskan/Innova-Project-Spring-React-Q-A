f2b591c2bd2542282f802217fa6db7ae33e16e9b
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-11 20:40:33 +0300

    Authentication-Login

--- Q-A/src/main/java/com/ws/Q/A/Application.java
+++ Q-A/src/main/java/com/ws/Q/A/Application.java
@@ -1,8 +1,13 @@
 package com.ws.Q.A;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.ws.Q.A.user.User;
+import com.ws.Q.A.user.UserService;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
@@ -10,5 +15,15 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return (args) -> {
+				User user = new User();
+				user.setUsername("user");
+				user.setDisplayName("display1");
+				user.setPassword("P4ssword");
+				userService.save(user);
+		};
+	}
 }

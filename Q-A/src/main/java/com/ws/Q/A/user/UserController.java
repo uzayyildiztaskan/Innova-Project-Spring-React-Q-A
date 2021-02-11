6925b93ab9cc37a5093a978a5438ec5818c05319
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-11 12:44:40 +0300

    Username confirmation and API error

--- Q-A/src/main/java/com/ws/Q/A/user/UserController.java
+++ Q-A/src/main/java/com/ws/Q/A/user/UserController.java
@@ -1,10 +1,16 @@
 package com.ws.Q.A.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ws.Q.A.error.ApiError;
 import com.ws.Q.A.shared.GenericResponse;
 
 @RestController
@@ -14,9 +20,19 @@ public class UserController {
 	UserService userService;
 	
 	@PostMapping("/api/1.0/users")
-	public GenericResponse registerUser(@RequestBody User user) {
+	public ResponseEntity<?> registerUser(@RequestBody User user) {
+		String userName = user.getUsername();
+		if(userName == null || userName.isEmpty()) {
+			
+			ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+			Map<String, String> validationErrors = new HashMap<>();
+			validationErrors.put("username", "Username cannot be null.");
+			error.setValidationErrors(validationErrors);
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
 		userService.save(user);
-		return new GenericResponse("User created");
+		return ResponseEntity.ok(new GenericResponse("User created"));
 	}
 
 }

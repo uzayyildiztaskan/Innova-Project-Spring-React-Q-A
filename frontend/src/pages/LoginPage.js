9bae3737191b0d7bf1060ae8b1babeb4cb7c2493
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-15 12:56:34 +0300

    Login page auth. error UI feedback

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -6,35 +6,55 @@ class LoginPage extends Component {
 
     state = {
         username: null,
-        password: null
-    }
+        password: null,
+        error: null
+    };
 
     onChange = event => {
         const { name, value } = event.target;
         this.setState({
-            [name] : value
-        })
-    }
+            [name] : value,
+            error: null
+        });
+    };
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const {username, password } = this.state;
         const creds = {
             username,
             password
+        };
+        this.setState({
+            error: null
+        });
+        try {
+            await login(creds)
+        } catch(apiError) {
+            this.setState({
+                error: apiError.response.data.message 
+            })
         }        
-        login(creds)
-    }
+    };
 
     render() {
+
+        const { username, password, error } = this.state;
+        const buttonEnabled = username && password;
+
         return (
             <div className = "container">
                 <form>
                     <h1 className = "text-center">Login</h1>
                     <Input label = "Username" name = "username" onChange = {this.onChange} />
                     <Input label = "Password" name = "password" type = 'password' onChange = {this.onChange} />
+                    {error && <div className = "alert alert-danger">{error}</div>}
                     <div className = "text-center">
-                        <button className = "btn btn-primary" onClick = {this.onClickLogin}>Login</button>
+                        <button className = "btn btn-primary" 
+                        onClick = {this.onClickLogin}
+                        disabled = {!buttonEnabled}>
+                            Login
+                        </button>
                     </div>                    
                 </form>                
             </div>

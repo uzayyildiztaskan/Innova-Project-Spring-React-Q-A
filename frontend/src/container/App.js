0696952de40afb2ceff171ab485c0817eb998ecf
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-16 16:05:26 +0300

    Added HomePage and UserPage

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ApiProgress from '../shared/ApiProgress';
 import RegisterUserPage from '../pages/RegisterUserPage';
 import LoginPage from '../pages/LoginPage';
+import HomePage from'../pages/HomePage';
+import UserPage from '../pages/UserPage';
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className = "row">
-      <div className = "col">
-        <RegisterUserPage />
-      </div>
-      <div className = "col">
-        <LoginPage />
-      </div>
+    <div>
+      <HashRouter>
+      <Switch>
+        <Route exact path = "/" component = {HomePage} />
+        <Route path = "/login" component = {LoginPage} />
+        <Route path = "/signup" component = {RegisterUserPage} />
+        <Route path = "/user/:username" component = {UserPage} />
+        <Redirect to = "/" /> 
+      </Switch>
+      </HashRouter>
   </div>
   );
 }

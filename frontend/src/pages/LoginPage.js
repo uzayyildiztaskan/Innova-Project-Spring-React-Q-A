69f5209370501b64df67751f1ffb8304e0b8ee5f
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-21 15:39:30 +0300

    Refactored with Hooks

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '../components/Input';
 import ButtonWithProgress from '../components/ButtonWithProgress';
 import { withApiProgress } from '../shared/ApiProgress';
@@ -6,74 +6,57 @@ import { connect } from 'react-redux';
 import { loginHandler, loginSuccess } from '../redux/authActions';
 // import { Authentication } from '../shared/AuthenticationContext';
 
-class LoginPage extends Component {
-
+const LoginPage = (props) => {
     // static contextType = Authentication;
 
-    state = {
-        username: null,
-        password: null,
-        error: null,
-        pendingApiCall: false
-    };
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
 
-    onChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name] : value,
-            error: null
-        });
-    };
+    useEffect(() => {
+        setError(undefined);
+    }, [username, password])
 
-    onClickLogin = async event => {
+    const onClickLogin = async event => {
         event.preventDefault();
-        const {username, password } = this.state;
         const creds = {
             username,
             password
         };
         
-        const { history, dispatch } = this.props;
-        const { push } = this.props.history;
+        const { history, dispatch } = props;
+        const { push } = history;
 
-        this.setState({
-            error: null
-        });
+        setError(undefined);
         try {
             await dispatch(loginHandler(creds))
             push('/');
         } catch(apiError) {
-            this.setState({
-                error: apiError.response.data.message 
-            });
+            setError(apiError.response.data.message);
         }        
     };
 
-    render() {
+    const { pendingApiCall } = props;
+    const buttonEnabled = username && password;
 
-        const { pendingApiCall } = this.props;
-        const { username, password, error } = this.state;
-        const buttonEnabled = username && password;
-
-        return (
-            <div className = "container">
-                <form>
-                    <h1 className = "text-center">Login</h1>
-                    <Input label = "Username" name = "username" onChange = {this.onChange} />
-                    <Input label = "Password" name = "password" type = 'password' onChange = {this.onChange} />
-                    {error && <div className = "alert alert-danger">{error}</div>}
-                    <div className = "text-center">
-                        <ButtonWithProgress 
-                        onClick = {this.onClickLogin} 
-                        disabled = {!buttonEnabled || pendingApiCall }
-                        pendingApiCall = { pendingApiCall }
-                        text = "Login" 
-                        />
-                    </div>                    
-                </form>                
-            </div>
-        );
-    }
+    return (
+        <div className = "container">
+            <form>
+                <h1 className = "text-center">Login</h1>
+                <Input label = "Username" onChange = {(event) => setUsername(event.target.value)} />
+                <Input label = "Password" type = 'password' onChange = {event => setPassword(event.target.value)} />
+                {error && <div className = "alert alert-danger">{error}</div>}
+                <div className = "text-center">
+                    <ButtonWithProgress 
+                    onClick = {onClickLogin} 
+                    disabled = {!buttonEnabled || pendingApiCall }
+                    pendingApiCall = { pendingApiCall }
+                    text = "Login" 
+                    />
+                </div>                    
+            </form>                
+        </div>
+    );
 }
 
 export default connect()(withApiProgress(LoginPage, '/api/1.0/auth'));

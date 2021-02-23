2bbc362f778691805eaaf499271a57c6cbeae377
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-23 16:01:19 +0300

    User inquiry and not found exception

--- Q-A/src/main/java/com/ws/Q/A/user/UserService.java
+++ Q-A/src/main/java/com/ws/Q/A/user/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ws.Q.A.error.NotFoundException;
+
 @Service
 public class UserService {
 	
@@ -28,6 +30,14 @@ public class UserService {
 			return userRepository.findByUsernameNot(user.getUsername(), page);
 		}
 		return userRepository.findAll(page);
+	}
+
+	public User getByUsername(String username) {
+		User inDB = userRepository.findByUsername(username);
+		if(inDB == null) {
+			throw new NotFoundException();
+		}
+		return inDB;
 	}
 	
 

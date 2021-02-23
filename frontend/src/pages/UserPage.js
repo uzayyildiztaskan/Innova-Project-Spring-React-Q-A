147b06a97948969301d1a29a36ef4ca16f19cb7d
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-23 17:18:14 +0300

    User not found UI feedback

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -5,8 +5,13 @@ import { useParams } from 'react-router-dom';
 
 const UserPage = () => {
     const [user, setUser] = useState();
+    const [notFound, setNotFound] = useState(false);
 
     const { username } = useParams();
+
+    useEffect(() => {
+        setNotFound(false);
+    }, [user]);
 
     useEffect(() => {
         const loadUser = async () => {
@@ -14,12 +19,24 @@ const UserPage = () => {
                 const response = await getUser(username);
                 setUser(response.data);
             }catch(error) {
-    
+                setNotFound(true);
             }
         };
         loadUser();
     }, [username]);
 
+    if (notFound) {
+        return (
+            <div className = "container">
+                <div className = "alert alert-danger text-center">
+                    <div>
+                    <span className = "material-icons" style = {{fontsize: '48px'}}>error</span>
+                    </div>
+                    User not found
+                </div>            
+            </div>
+        );
+    }
 
     return (
         <div className = "container">

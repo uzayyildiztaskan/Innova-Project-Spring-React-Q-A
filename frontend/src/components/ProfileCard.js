92c91287883171c4e6a6703eaa85b2fd503496b4
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-23 23:17:36 +0300

    Added edit&cancel options for display name

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
@@ -6,11 +6,26 @@ import Input from'./Input';
 
 const ProfileCard = (props) => {
     const [inEditMode, setInEditMode] = useState(false);
+    const [updatedDisplayName, setUpdatedDisplayName] = useState();
     const { username: loggedInUsername} = useSelector((store) => ({username: store.username}));
     const routeParams = useParams();
-
+    
     const { user } = props;
     const {username, displayName, image } = user;
+    
+    useEffect(() => {
+        if (!inEditMode) {
+            setUpdatedDisplayName(undefined);
+        } else {
+            setUpdatedDisplayName(displayName);
+        }
+        setUpdatedDisplayName(undefined);
+    }, [inEditMode, displayName]);
+
+    const onClickSave = () => {
+        
+    }
+
 
     const pathUsername = routeParams.username;
     let message = "Edit disabled";
@@ -37,9 +52,9 @@ const ProfileCard = (props) => {
                 )}
                 {inEditMode && (
                     <div>
-                        <Input label = "Change Display Name"/>
+                        <Input label = "Change Display Name" defaultValue = {displayName} onChange = {(event) => {setUpdatedDisplayName(event.target.value)}}/>
                         <div>
-                            <button className = "btn btn-primary d-inline-flex">
+                            <button className = "btn btn-primary d-inline-flex" onClick = {onClickSave}>
                                 <span className = "material-icons">save</span>Save
                             </button>
                             <button className = "btn btn-light d-inline-flex ml-1" onClick = {() => setInEditMode(false)}>

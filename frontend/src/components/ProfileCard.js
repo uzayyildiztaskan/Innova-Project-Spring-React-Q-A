4fbba42198225a9bea46739422692aa592eb33bb
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-23 22:59:04 +0300

    User profile edit mode

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import Input from'./Input';
 
 const ProfileCard = (props) => {
+    const [inEditMode, setInEditMode] = useState(false);
     const { username: loggedInUsername} = useSelector((store) => ({username: store.username}));
     const routeParams = useParams();
 
@@ -22,9 +24,30 @@ const ProfileCard = (props) => {
                 <ProfileImageWithDefault className = "rounded-circle shadow" width = "200" height = "200" alt = {`${username} profile`} image = {image}/>
             </div>
             <div className = "card-body">
-                <h3>
-                    {displayName}@{username}
-                </h3>
+                {!inEditMode && (
+                    <>
+                        <h3>
+                            {displayName}@{username}
+                        </h3>
+                        <button className = "btn btn-success d-inline-flex" onClick = {() => setInEditMode(true)}>
+                            <span className = "material-icons">edit</span>
+                            Edit
+                        </button>
+                    </>
+                )}
+                {inEditMode && (
+                    <div>
+                        <Input label = "Change Display Name"/>
+                        <div>
+                            <button className = "btn btn-primary d-inline-flex">
+                                <span className = "material-icons">save</span>Save
+                            </button>
+                            <button className = "btn btn-light d-inline-flex ml-1" onClick = {() => setInEditMode(false)}>
+                                <span className = "material-icons">close</span>Cancel
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );

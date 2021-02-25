c60722731a1bbc468d0a1ceb53c29595a09fd95a
uyildiztaskan <yildiztaskan007@gmail.com>
2021-02-26 01:06:32 +0300

    Added drop down menu to top bar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -3,12 +3,15 @@ import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutSuccess } from '../redux/authActions';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
 
 const TopBar = (props) => {
 
-    const {username, isLoggedIn} = useSelector((store) => ({     
+    const { username, isLoggedIn, displayName, image } = useSelector((store) => ({     
             isLoggedIn: store.isLoggedIn,
-            username: store.username        
+            username: store.username,
+            displayName: store.displayName,
+            image: store.image        
         }));
 
     const dispatch = useDispatch();
@@ -34,12 +37,22 @@ const TopBar = (props) => {
         if (isLoggedIn) {
             links = (
                 <ul className = "navbar-nav ml-auto">
-                    <li>
-                        <Link className = "nav-link" to = {`/user/${username}`}>
-                            {username}
-                        </Link>
+                    <li className = "nav-item dropdown">
+                        <div className = "d-flex" style = {{cursor: 'pointer'}}>
+                            <ProfileImageWithDefault image = {image} width = "32" height = "32" className = "rounded-circle m-auto" />
+                            <span className = "nav-link dropdown-toggle">{displayName}</span>
+                        </div>
+                        <div className = "dropdown-menu show p-0 shadow">
+                            <Link className = "dropdown-item d-flex p-2" to = {`/user/${username}`}>
+                            <span className = "material-icons text-info mr-2">person</span>
+                                My Profile
+                            </Link>
+                            <span className = "dropdown-item d-flex p-2" onClick = {onLogoutSuccess} style = {{cursor: 'pointer'}}>
+                                <span className = "material-icons text-danger mr-2">power_settings_new</span>
+                                Logout
+                            </span>
+                        </div>
                     </li>
-                    <li className = "nav-link" onClick = {onLogoutSuccess} style = {{cursor: 'pointer'}}>Logout</li>
                 </ul>
             );
         }
